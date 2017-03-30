b49f2f00357ebd48e905820109a2f9efc023d1c8
Jaclyn <jaclyn.d.bryant@gmail.com>
2017-03-30 02:04:23 +0000

    Fixed an error in Encounter.js, imported Menu.js

--- src/components/encounter/Encounter.js
+++ src/components/encounter/Encounter.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+
 import Scene from "../scene/Scene";
 import Battlefield from "../battlefield/Battlefield";
 import Party from "../party/Party";
+import Menu from "../menu/Menu";
+
 import dune from "../../images/reddune.jpg";
 
 class Encounter extends Component {

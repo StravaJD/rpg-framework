f1f0807c6409f96671ff3a81877901816bc91a17
Yukiiro-Nite <e.kazuma@gmail.com>
2017-03-30 02:00:37 +0000

    Finished initializing the component files

---
+++ src/components/battlefield/Battlefield.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Battlefield extends Component {
+  render(){
+    <div>
+    
+    </div>
+  }
+}
+
+export default Battlefield;
--- src/components/encounter/Encounter.js
+++ src/components/encounter/Encounter.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Scene from "../scene/Scene";
+import Battlefield from "../battlefield/Battlefield";
+import Party from "../party/Party";
 import dune from "../../images/reddune.jpg";
 
 class Encounter extends Component {
   render() {
     return (
       <Scene image={dune}>
+        <Battlefield>
+          <Party />
+          <Party />
+        </Battlefield>
+        <Menu />
       </Scene>
     )
   }
